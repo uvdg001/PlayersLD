e3f5e512479f55aa5a13fc973dcbb88dc3cda4ee
uvdg001 <uvdg.001@gmail.com>
2025-11-21 19:33:42 -0300

    Update vite.config.ts
    21 viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,20 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, (process as any).cwd(), '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ""),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ""),
-        'FIREBASE_CONFIG_JSON': JSON.stringify(env.FIREBASE_CONFIG_JSON || "")
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './src'),
-        }
+export default defineConfig({
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       }
-    };
+    }
 });
